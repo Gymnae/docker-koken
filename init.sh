74936ba0e8ca8df704296f6287211bbfb6174fa5
Gymnae <dagorwaith@gmail.com>
2016-03-12 11:11:07 +0000

    cleaning up init script

--- init.sh
+++ init.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-################################################################
-# The following should be run anytime the container is booted, #
-# incase host is resized                                       #
-################################################################
-
 # Set PHP pools to take up to 1/2 of total system memory total, split between the two pools.
 # 30MB per process is conservative estimate, is usually less than that
 PHP_MAX=$(expr $(grep 'MemTotal' /proc/meminfo | sed -e 's/MemTotal://' -e 's/ kB//') / 1024 / 2 / 30 / 2)
@@ -14,25 +9,6 @@ sed -i -e"s/pm.max_children = 5/pm.max_children = $PHP_MAX/" /etc/php/php.ini
 # Set nginx worker processes to equal number of CPU cores
 sed -i -e"s/worker_processes\s*4/worker_processes $(cat /proc/cpuinfo | grep processor | wc -l)/" /etc/nginx/nginx.conf
 
-# You may add here your
-# server {
-#	...
-# }
-# statements for each of your virtual hosts to this file
-
-
-##
-# You should look at the following URL's in order to grasp a solid understanding
-# of Nginx configuration files in order to fully unleash the power of Nginx.
-# http://wiki.nginx.org/Pitfalls
-# http://wiki.nginx.org/QuickStart
-# http://wiki.nginx.org/Configuration
-#
-# Generally, you will want to move this file somewhere, and start with a clean
-# file but keep this around for reference. Or just disable in sites-enabled.
-#
-# Please see /usr/share/doc/nginx-doc/examples/ for more detailed examples.
-##
 
 # start php-fpm
 mkdir -p /media/koken/logs/php-fpm
@@ -44,4 +20,3 @@ mkdir -p /tmp/nginx
 chown nginx /tmp/nginx
 nginx
 
-#find /media/koken/www/storage/cache/images/* -atime +10 -exec rm {} \;
